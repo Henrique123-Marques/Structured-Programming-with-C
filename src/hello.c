6d70bebbb84347c8c806731d1d0cf069bea338c6
Henrique Marques Santos Silva <henriquemarquessantossilva@hotmail.com>
2025-10-22 22:55:15 -0300

    ponteiros_alocacao

--- src/hello.c
+++ src/hello.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(){
-    float n, soma = 0.0;
-    int i = 1;
-    do{
-        n = printf("Digite valores positivos ou -1 para sair:\n");
-        scanf("%f", &n);
-        if (i >= 0){
-           soma += n;
-           i++;
-        }
-    }while(n != -1);
-    if (i > 0){
-        float media = soma / i;
-        printf("A media eh:%.6f\n", media);
-    } else {
-        printf("Nenhum valor digitado.\n");
+    double *v1, *v2, produtoEscalar;
+    int n, i;
+
+    scanf("%d", &n);
+    v1 = malloc(n * sizeof(double));
+    v2 = malloc(n * sizeof(double));
+
+    for (i = 0; i < n; i++){
+        scanf("%lf", &v1[i]);
     }
+    for (i = 0; i < n; i++){
+        scanf("%lf", &v2[i]);
+    }
+
+    produtoEscalar = 0;
+
+    for (i = 0; i < n; i++){
+        produtoEscalar = produtoEscalar + (v1[i] * v2[i]);
+    }
+
+    printf("Resposta: %.2lf\n", produtoEscalar);
+    free(v1);
+    free(v2);
     return 0;
 }
+
